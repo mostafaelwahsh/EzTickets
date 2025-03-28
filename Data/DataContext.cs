d9f95c0dc159c3b496c22cc9070123686a63f36b
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-03-29 00:58:15 +0200

    Remove Class1 and add DataContext with factory
    - Deleted `Class1.cs`.
    - Updated `Data.csproj` with new package references and added a project reference to `Models.csproj`.
    - Modified `EzTicket.csproj` to include project references to `Data.csproj` and `Models.csproj`.
    - Introduced `DataContext.cs` to manage entities with `DbSet` properties.
    - Added `DataContextFactory.cs` for design-time DbContext creation using configuration from `appsettings.json`.

---
+++ Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Models;
+
+namespace Data
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // DbSet properties for your entities
+        public DbSet<Category> Courses { get; set; }
+        public DbSet<Event> Plugins { get; set; }
+        public DbSet<EventCategory> CourseContents { get; set; }
+        public DbSet<Order> Enrollments { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Ticket> ShoppingCarts { get; set; }
+    }
+    
+}
---
+++ Data/DataContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "EzTicket");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+                        optionsBuilder.UseSqlServer(config.GetConnectionString("CS"));
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
