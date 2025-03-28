de4920017f0fa73ac46c248bb46fbbad7036501e
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-03-29 01:04:19 +0200

    Add AutoMapper and JWT Bearer Authentication
    Updated EzTicket.csproj to include AutoMapper v14.0.0 and Microsoft.AspNetCore.Authentication.JwtBearer v8.0.14. Added new FakePaymentService class with a placeholder for payment processing logic. Introduced MappingProfile class for AutoMapper configuration with necessary using directives.

---
+++ EzTicket/Services/FakePaymentService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EzTicket.Services
+{
+    public class FakePaymentService
+    {
+        private static readonly Random _random = new();
+
+        //public bool ProcessPayment(PaymentViewModel payment)
+        //{
+        //    // Simulate failure in 5% of cases 
+        //    return _random.Next(1, 101) > 5;
+        //}
+    }
+}
---
+++ EzTicket/Services/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Models;
+
+namespace EzTicket.Services
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
