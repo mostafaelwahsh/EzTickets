9a12d282a18248e15a1b4eef1e12030f6d6628ff
mostafaelwahsh <mostafanabil447@gmail.com>
2025-03-29 04:45:43 +0200

    adding a key to eventcategory

--- Model/EventCategory.cs
+++ Model/EventCategory.cs
@@ -1,7 +1,10 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
 {
     public class EventCategory
     {
+        [Key]
         public string EventId { get; set; }
         public Event Event { get; set; }
         public string CategoryId { get; set; }
