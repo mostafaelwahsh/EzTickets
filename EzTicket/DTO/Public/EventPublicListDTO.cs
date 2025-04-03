800c645a5300d46746d7dd03d86ff7cffc8aa3f1
HazemRashad <hazemrashad55@gmail.com>
2025-04-03 20:40:36 +0200

    Add EventPublicListDTO class for event details
    Introduced the EventPublicListDTO class with properties
    such as EventID, EventName, VenueName, City, Country,
    StartDate, PricePerTicket, and nullable ImageUrl.
    Updated namespace to include 'using Models;'.

--- EzTicket/DTO/Public/EventPublicListDTO.cs
+++ EzTicket/DTO/Public/EventPublicListDTO.cs
@@ -1,6 +1,17 @@
-﻿namespace EzTickets.DTO.Public
+﻿using Models;
+
+namespace EzTickets.DTO.Public
 {
     public class EventPublicListDTO
     {
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public string VenueName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime StartDate { get; set; }
+        public decimal PricePerTicket { get; set; }
+        public string? ImageUrl { get; set; }
+        public EventCategoryType Category { get; set; }
     }
 }
