d8d81414ff9dd877817e45f30274b2a2d236d315
HazemRashad <hazemrashad55@gmail.com>
2025-04-04 21:55:36 +0200

    Add HomeController for event management
    Introduces a new `HomeController` in the `EzTickets.Controllers` namespace to handle HTTP requests for public events. Implements dependency injection for `IEventRepository` and `IMapper`. Adds `GetAllEventsPublic` method to retrieve paginated public events, returning a structured `GeneralResponse` with success status and event data. Includes exception handling for robust error management.

---
+++ EzTicket/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using EzTickets.DTO.Pagination;
+using EzTickets.DTO.Public;
+using EzTickets.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EzTickets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+        private readonly IMapper _mapper;
+
+        public HomeController(IEventRepository eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public ActionResult<GeneralResponse> GetAllEventsPublic([FromQuery] PaginationParams pagination)
+        {
+            try
+            {
+                var pagedEvents = _eventRepository.GetAllPublic(pagination);
+                var result = new PagedResponse<EventPublicListDTO>(
+                    _mapper.Map<List<EventPublicListDTO>>(pagedEvents.Data),
+                    pagedEvents.PageNumber,
+                    pagedEvents.PageSize,
+                    pagedEvents.TotalRecords);
+
+                return new GeneralResponse
+                {
+                    IsPass = true,
+                    Data = result
+                };
+            }
+            catch (Exception ex)
+            {
+                return new GeneralResponse
+                {
+                    IsPass = false,
+                };
+            }
+        }
+    }
+}
