13f382d8ff2bce9f08394cba33c130201d23a4f0
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-04-05 00:32:32 +0200

    Update Order entity and related logic for tickets
    - Change `PaymentMethod` to nullable in `Order` entity.
    - Add `NumberOfTickets` property to order creation in `OrderController`.
    - Update `CreateOrderResponseDTO` to include `NumberOfTickets`.
    - Reflect `NumberOfTickets` in the `Order` class.
    - Retain `CreatedAt` property for order creation timestamp.

---
+++ Data/Migrations/20250404221911_nullpaymentmethod.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullpaymentmethod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PaymentMethod",
+                table: "Order",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PaymentMethod",
+                table: "Order",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+        }
+    }
+}
